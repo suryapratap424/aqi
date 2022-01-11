444fa1ffbf1471fe92a219801357a4e4e1a00d95
Surya Pratap Singh <suryaprataps424@gmail.com>
2022-01-11 15:30:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT||80;
 const axios = require("axios")
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/data',async(req,res)=>{
-    console.log(req.query)
+    // console.log(req.query)
     let re = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${req.query.lat}&lon=${req.query.lon}&appid=${process.env.APIKEY}`)
     .catch(e=>console.log(e))
     // console.log(re.data)
