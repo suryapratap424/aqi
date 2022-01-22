8d7ebf9de6a7e644e908f7bf100c43a1c2ac9751
Surya Pratap Singh <suryaprataps424@gmail.com>
2022-01-22 11:41:59 +0530

    added dynamic color

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-const myMap = L.map("map").setView([28.5915128, 77.2192949], 10);
+const myMap = L.map("map").setView([23, 80], 5);
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 const tileLayer = L.tileLayer(tileUrl, {
   attribution,
-  minZoom: 5,
+  minZoom: 2,
   noWrap: true,
 });
 tileLayer.addTo(myMap);
@@ -38,8 +38,9 @@ function showDataOnMap(stations) {
       .then((r) => r.json())
       .then((x) => {
         // background-color: ${color(x.list[0].main.aqi)};
+        // background-color: ${color(x.list[0].main.aqi)};
         let style = `
-        background-color: ${color(aqi(x.list[0].components))};
+        background-color: ${color(x.list[0].main.aqi)};
         width:2rem;
         height:2rem;
         display: flex;
@@ -60,156 +61,158 @@ function showDataOnMap(stations) {
           });
         layer.addLayer(d);
       })
-      .catch((e) => (document.getElementById("comp").innerHTML = e));
+      .catch((e) => (console.log(e)));
   });
 
   layer.addTo(myMap);
 }
 showDataOnMap(stations);
 //-------------------------aqi-------------------------------------------------
-function color(c){
-  if(c<=700){
-    return 'green'
-  }
-  if(c<800){
-    return 'greenyellow'
-  }
-  if(c<=850){
-    return 'yellow'
-  }
-  if(c<=900){
-    return 'orange'
-  }
-  if(c>900){
-    return 'red'
-  }
-}
 // function color(c){
-//   if(c==1){
+//   if(c<=700){
 //     return 'green'
 //   }
-//   if(c==2){
+//   if(c<800){
 //     return 'greenyellow'
 //   }
-//   if(c==3){
+//   if(c<=850){
 //     return 'yellow'
 //   }
-//   if(c==4){
+//   if(c<=900){
 //     return 'orange'
 //   }
-//   if(c==5){
+//   if(c>900){
 //     return 'red'
 //   }
 // }
+function color(c){
+  if(c==1){
+    return 'green'
+  }
+  if(c==2){
+    return 'greenyellow'
+  }
+  if(c==3){
+    return 'yellow'
+  }
+  if(c==4){
+    return 'orange'
+  }
+  if(c==5){
+    return 'red'
+  }
+}
 function aqi(d){
-  let {pm10,pm2_5,so2,no,no2,co,o3,nh3} = d
-  co = co / 1000
-  if(pm10<=50)
-  pm10=pm10
-  if(pm10>50&&pm10<=100)
-  pm10=pm10
-  if(pm10>100&&pm10<=250)
-  pm10=100+(pm10-100)*100/150
-  if(pm10>250&&pm10<=350)
-  pm10=200+(pm10-250)
-  if(pm10>350&&pm10<=430)
-  pm10=300+(pm10-350)*(100/80)
-  if(pm10>430)
-  pm10=400+(pm10-430)*(100/80)
-  //---------------------------------
-  if(pm2_5<=30)
-  pm2_5=pm2_5*50/30
-  if(pm2_5>30&&pm2_5<=60)
-  pm2_5=50+(pm2_5-30)*50/30
-  if(pm2_5>60&&pm2_5<=90)
-  pm2_5=100+(pm2_5-60)*100/30
-  if(pm2_5>90&&pm2_5<=120)
-  pm2_5=200+(pm2_5-90)*(100/30)
-  if(pm2_5>120&&pm2_5<=250)
-  pm2_5=300+(pm2_5-120)*(100/130)
-  if(pm2_5>250)
-  pm2_5=400+(pm2_5-250)*(100/130)
-  //---------------------------------
-  if(so2<=40)
-  so2=so2*50/40
-  if(so2>40&&so2<=80)
-  so2=50+(so2-40)*50/40
-  if(so2>80&&so2<=380)
-  so2=100+(so2-80)*100/300
-  if(so2>380&&so2<=800)
-  so2=200+(so2-380)*(100/420)
-  if(so2>800&&so2<=1600)
-  so2=300+(so2-800)*(100/800)
-  if(so2>1600)
-  so2=400+(so2-1600)*(100/800)
-  //---------------------------------
-  if(no<=40)
-  no=no*50/40
-  if(no>40&&no<=80)
-  no=50+(no-40)*50/40
-  if(no>80&&no<=180)
-  no=100+(no-80)*100/100
-  if(no>180&&no<=280)
-  no=200+(no-180)*(100/100)
-  if(no>280&&no<=400)
-  no=300+(no-280)*(100/120)
-  if(no>400)
-  no=400+(no-400)*(100/120)
-  //---------------------------------
-  if(no2<=40)
-  no2=no2*50/40
-  if(no2>40&&no2<=80)
-  no2=50+(no2-40)*50/40
-  if(no2>80&&no2<=180)
-  no2=100+(no2-80)*100/100
-  if(no2>180&&no2<=280)
-  no2=200+(no2-180)*(100/100)
-  if(no2>280&&no2<=400)
-  no2=300+(no2-280)*(100/120)
-  if(no2>400)
-  no2=400+(no2-400)*(100/120)
-  //---------------------------------
-  if(co<=1)
-  co=co*50/1
-  if(co>1&&co<=2)
-  co=50+(co-1)*50/1
-  if(co>2&&co<=10)
-  co=100+(co-2)*100/8
-  if(co>10&&co<=17)
-  co=200+(co-10)*(100/7)
-  if(co>17&&co<=34)
-  co=300+(co-17)*(100/17)
-  if(co>34)
-  co=400+(co-34)*(100/17)
-  //---------------------------------
-  if(o3<=50)
-  o3=o3*50/50
-  if(o3>50&&o3<=100)
-  o3=50+(o3-50)*50/50
-  if(o3>100&&o3<=168)
-  o3=100+(o3-100)*100/68
-  if(o3>168&&o3<=208)
-  o3=200+(o3-168)*(100/40)
-  if(o3>208&&o3<=748)
-  o3=300+(o3-208)*(100/539)
-  if(o3>748)
-  o3=400+(o3-400)*(100/539)
-  //---------------------------------
-  if(nh3<=200)
-  nh3=nh3*50/200
-  if(nh3>200&&nh3<=400)
-  nh3=50+(nh3-200)*50/200
-  if(nh3>400&&nh3<=800)
-  nh3=100+(nh3-400)*100/400
-  if(nh3>800&&nh3<=1200)
-  nh3=200+(nh3-800)*(100/400)
-  if(nh3>1200&&nh3<=1800)
-  nh3=300+(nh3-1200)*(100/600)
-  if(nh3>1800)
-  nh3=400+(nh3-1800)*(100/600)
-  //---------------------------------
-  console.log(pm10,pm2_5,so2,no,no2,co,o3,nh3)
-  return Math.round(Math.max(pm10,pm2_5,so2,no,no2,co,o3,nh3))
+  let {pm10,pm2_5} = d
+  // let {pm10,pm2_5,so2,no,no2,co,o3,nh3} = d
+  // co = co / 1000
+  // if(pm10<=50)
+  // pm10=pm10
+  // if(pm10>50&&pm10<=100)
+  // pm10=pm10
+  // if(pm10>100&&pm10<=250)
+  // pm10=100+(pm10-100)*100/150
+  // if(pm10>250&&pm10<=350)
+  // pm10=200+(pm10-250)
+  // if(pm10>350&&pm10<=430)
+  // pm10=300+(pm10-350)*(100/80)
+  // if(pm10>430)
+  // pm10=400+(pm10-430)*(100/80)
+  // //---------------------------------
+  // if(pm2_5<=30)
+  // pm2_5=pm2_5*50/30
+  // if(pm2_5>30&&pm2_5<=60)
+  // pm2_5=50+(pm2_5-30)*50/30
+  // if(pm2_5>60&&pm2_5<=90)
+  // pm2_5=100+(pm2_5-60)*100/30
+  // if(pm2_5>90&&pm2_5<=120)
+  // pm2_5=200+(pm2_5-90)*(100/30)
+  // if(pm2_5>120&&pm2_5<=250)
+  // pm2_5=300+(pm2_5-120)*(100/130)
+  // if(pm2_5>250)
+  // pm2_5=400+(pm2_5-250)*(100/130)
+  // //---------------------------------
+  // if(so2<=40)
+  // so2=so2*50/40
+  // if(so2>40&&so2<=80)
+  // so2=50+(so2-40)*50/40
+  // if(so2>80&&so2<=380)
+  // so2=100+(so2-80)*100/300
+  // if(so2>380&&so2<=800)
+  // so2=200+(so2-380)*(100/420)
+  // if(so2>800&&so2<=1600)
+  // so2=300+(so2-800)*(100/800)
+  // if(so2>1600)
+  // so2=400+(so2-1600)*(100/800)
+  // //---------------------------------
+  // if(no<=40)
+  // no=no*50/40
+  // if(no>40&&no<=80)
+  // no=50+(no-40)*50/40
+  // if(no>80&&no<=180)
+  // no=100+(no-80)*100/100
+  // if(no>180&&no<=280)
+  // no=200+(no-180)*(100/100)
+  // if(no>280&&no<=400)
+  // no=300+(no-280)*(100/120)
+  // if(no>400)
+  // no=400+(no-400)*(100/120)
+  // //---------------------------------
+  // if(no2<=40)
+  // no2=no2*50/40
+  // if(no2>40&&no2<=80)
+  // no2=50+(no2-40)*50/40
+  // if(no2>80&&no2<=180)
+  // no2=100+(no2-80)*100/100
+  // if(no2>180&&no2<=280)
+  // no2=200+(no2-180)*(100/100)
+  // if(no2>280&&no2<=400)
+  // no2=300+(no2-280)*(100/120)
+  // if(no2>400)
+  // no2=400+(no2-400)*(100/120)
+  // //---------------------------------
+  // if(co<=1)
+  // co=co*50/1
+  // if(co>1&&co<=2)
+  // co=50+(co-1)*50/1
+  // if(co>2&&co<=10)
+  // co=100+(co-2)*100/8
+  // if(co>10&&co<=17)
+  // co=200+(co-10)*(100/7)
+  // if(co>17&&co<=34)
+  // co=300+(co-17)*(100/17)
+  // if(co>34)
+  // co=400+(co-34)*(100/17)
+  // //---------------------------------
+  // if(o3<=50)
+  // o3=o3*50/50
+  // if(o3>50&&o3<=100)
+  // o3=50+(o3-50)*50/50
+  // if(o3>100&&o3<=168)
+  // o3=100+(o3-100)*100/68
+  // if(o3>168&&o3<=208)
+  // o3=200+(o3-168)*(100/40)
+  // if(o3>208&&o3<=748)
+  // o3=300+(o3-208)*(100/539)
+  // if(o3>748)
+  // o3=400+(o3-400)*(100/539)
+  // //---------------------------------
+  // if(nh3<=200)
+  // nh3=nh3*50/200
+  // if(nh3>200&&nh3<=400)
+  // nh3=50+(nh3-200)*50/200
+  // if(nh3>400&&nh3<=800)
+  // nh3=100+(nh3-400)*100/400
+  // if(nh3>800&&nh3<=1200)
+  // nh3=200+(nh3-800)*(100/400)
+  // if(nh3>1200&&nh3<=1800)
+  // nh3=300+(nh3-1200)*(100/600)
+  // if(nh3>1800)
+  // nh3=400+(nh3-1800)*(100/600)
+  // //---------------------------------
+  // console.log(pm10,pm2_5,so2,no,no2,co,o3,nh3)
+  return Math.round(Math.max(pm10,pm2_5))
+  // return Math.round(Math.max(pm10,pm2_5,so2,no,no2,co,o3,nh3))
 }
         
 //-----------------------------------------------------------------------------
@@ -225,7 +228,7 @@ function filldata(data) {
     )
     .join("")}
     </ul>`;
-  document.getElementById("data").innerHTML = html;
+  // document.getElementById("data").innerHTML = html;
   let card = document.getElementById(`${data.coord.lat}`);
   if (card) card.innerHTML = html;
 }
@@ -238,7 +241,7 @@ fetch(`data?lat=50&lon=50`)
   .catch((e) => (document.getElementById("comp").innerHTML = e));
 
 function onclick(e) {
-  document.getElementById("comp").innerHTML = `Loading....`;
+  // document.getElementById("comp").innerHTML = `Loading....`;
   let lat = e.latlng.lat;
   let lng = e.latlng.lng;
   fetch(`data?lat=${lat}&lon=${lng}`)
@@ -247,5 +250,5 @@ function onclick(e) {
       console.log(x);
       filldata(x);
     })
-    .catch((e) => (document.getElementById("comp").innerHTML = e));
+    .catch((e) => console.log(e));
 }
